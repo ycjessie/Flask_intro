949d05a4d782bffb78b1fec3309a62f044135e06
Jessie Chen <yhcjessie1@gmail.com>
2020-12-07 14:58:08 -0500

    set up the GET and POST route

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 #g global, a global access to db for the app
 from flask import Flask,jsonify,g
+#=======import CORS module=============
+from flask_cors import CORS
+#=========import dog file=============
+from resources.dogs import dog # adding this line
+#===============import models===============
 import models
 DEBUG = True
 PORT = 8000
@@ -18,22 +23,28 @@ def after_request(response):
     """Close the database connection after each request."""
     g.db.close()
     return response
+#==================Middleware=======================
+CORS(dog, origins=['http://localhost:3000'], supports_credentials=True) # adding this line
+#register blueprint(Route) define a prefix, 
+# like Express app.use('/api/v1/dogs', require('dogsControllers.js'))
+app.register_blueprint(dog, url_prefix='/api/v1/dogs') # adding this line
 
 # The default URL ends in / ("my-website.com/").
 @app.route('/')
 def index():
-    return 'Hello World!'
+    # return 'Hello World!'
 
-#   my_list = ["Hey", "check", "this", "out"]
-#   return my_list[0] # Works!
+  my_list = ["Hey", "check", "this", "out"]
+  return my_list[0] # Works!
 #json
-@app.route('/json')
-def dog():
-    return jsonify(name="Frankie", age=8)
+# @app.route('/json')
+# def dog():
+#     return jsonify(name="Frankie", age=8)
 #variables
 @app.route('/sayhi/<username>') # When someone goes here...
 def hello(username): # Do this.
     return "Hello {}".format(username)    
+
 # Run the app when the program starts!
 if __name__ == '__main__':
     models.initialize()
--- resources/dogs.py
+++ resources/dogs.py
@@ -1,10 +1,36 @@
 import models
-
+#https://git.generalassemb.ly/prudential-0921/flask-intro-get-post#setting-up-our-controller-aka-a-resource
+#Blueprint module: setup Routes(Express)
 from flask import Blueprint, jsonify, request
-
+#peewee,http://docs.peewee-orm.com/en/latest/peewee/playhouse.html
 from playhouse.shortcuts import model_to_dict
 
 
 # first argument is blueprints name
 # second argument is it's import_name
 dog = Blueprint('dogs', 'dog')
+#set up the GET and POST route
+@dog.route('/', methods=["GET"])
+def get_all_dogs():
+    ## find the dogs and change each one to a dictionary into a new array
+    try:
+        #list comprehension python[]
+        dogs = [model_to_dict(dog) for dog in models.Dog.select()]
+        print(dogs)#like console.log
+        return jsonify(data=dogs, status={"code": 200, "message": "Success"})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
+@dog.route('/', methods=["POST"])
+def create_dogs():
+    ## see request payload anagolous to req.body in express
+    payload = request.get_json()
+    print(type(payload), 'payload')
+    dog = models.Dog.create(**payload)
+    ## see the object
+    print(dog.__dict__)
+    ## Look at all the methods
+    print(dir(dog))
+    # Change the model to a dict
+    print(model_to_dict(dog), 'model to dict')
+    dog_dict = model_to_dict(dog)
+    return jsonify(data=dog_dict, status={"code": 201, "message": "Success"})
