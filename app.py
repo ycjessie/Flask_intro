953e53aae3d4e671340b695d51242a42559afaf7
Jessie Chen <yhcjessie1@gmail.com>
2020-12-07 11:36:58 -0500

    createdb and py file for flask-dog-app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 DEBUG = True
 PORT = 8000
@@ -12,6 +12,16 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
+#   my_list = ["Hey", "check", "this", "out"]
+#   return my_list[0] # Works!
+#json
+@app.route('/json')
+def dog():
+    return jsonify(name="Frankie", age=8)
+#variables
+@app.route('/sayhi/<username>') # When someone goes here...
+def hello(username): # Do this.
+    return "Hello {}".format(username)    
 # Run the app when the program starts!
 if __name__ == '__main__':
     app.run(debug=DEBUG, port=PORT)
---
+++ models.py
@@ -0,0 +1,22 @@
+# https://git.generalassemb.ly/prudential-0921/flask-intro-get-post
+# import * means import everything from peewee
+from peewee import *
+import datetime
+
+# Connect to a Postgres database.
+DATABASE = PostgresqlDatabase('flask_dog_app', host='localhost', port=5432)
+
+class Dog(Model):
+    name = CharField()
+    owner = CharField()
+    breed = CharField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Dog], safe=True)
+    print("TABLES Created")
+    DATABASE.close()
